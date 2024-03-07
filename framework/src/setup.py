28b573fac3cdc8aef27f9209cc3c800433b1e833
Will Ingram <mingmangram@gmail.com>
2024-03-07 12:07:00 -0700

    added to setup

--- framework/src/setup.py
+++ framework/src/setup.py
@@ -1,3 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(name='deepteddy')
+setuptools.setup(
+    name='deepteddy',
+    version='0.0.7',
+    author='Will Ingram',
+    author_email='mingmangram@gmail.com',
+    description='A mini deep learning framework made to teach myself ML and Python',
+    url='https://github.com/wingram3/ml_framework',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
